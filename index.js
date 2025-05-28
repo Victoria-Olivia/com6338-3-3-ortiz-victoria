31d8d104a67faa454757019c464b3e199aa85fd2
Victoria Ortiz <ortiz.victoria@ufl.edu>
2025-05-28 04:36:20 -0400

    Fixed punctuation in quiz questions

--- index.js
+++ index.js
@@ -1,37 +1,35 @@
 // Your code here
 var questionsArr = [
       {
-        question: 'JavaScript is super fun to learn? Click OK for True, Cancel for False',
+        question: 'JavaScript is super fun to learn? Click OK for True, Cancel for False.',
         answer: true
       },
       {
-        question: 'The Earth is flatter than paper? Click OK for True, Cancel for False',
+        question: 'The Earth is flatter than paper? Click OK for True, Cancel for False.',
         answer: false
       }, 
       {
-        question: 'Study hard and you will succeed? Click OK for True, Cancel for False',
+        question: 'Study hard and you will succeed? Click OK for True, Cancel for False.',
         answer: true
       }, 
       {
-        question: 'DaVinci believed that simplicity is the ultimate sophistication”? Click OK for True, Cancel for False',
+        question: 'DaVinci believed that simplicity is the ultimate sophistication”? Click OK for True, Cancel for False.',
         answer: true
       }, 
       {
-        question: 'E.T. is a friendly alien that wants to phone home? Click OK for True, Cancel for False',
+        question: 'E.T. is a friendly alien that wants to phone home? Click OK for True, Cancel for False.',
         answer: true
       }, 
       {
-        question: 'HTML stands for HyperText Markup Language? Click OK for True, Cancel for False',
+        question: 'HTML stands for HyperText Markup Language? Click OK for True, Cancel for False.',
         answer: true
       }, 
       {
-        question: 'Python is a type of snake? (tricky one) Click OK for True, Cancel for False',
+        question: 'Python is a type of snake? (tricky one) Click OK for True, Cancel for False.',
         answer: true
       }, 
       {
-        question: 'CSS stands for Cascading Style Sheets? Click OK for True, Cancel for False',
+        question: 'CSS stands for Cascading Style Sheets? Click OK for True, Cancel for False.',
         answer: true
       }, 
 ];
-
-function runQuiz () {}
