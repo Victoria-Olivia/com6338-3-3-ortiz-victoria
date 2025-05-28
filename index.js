59d22a0ff75cf3612cfb23597ad063a3b1a59410
Victoria Ortiz <ortiz.victoria@ufl.edu>
2025-05-28 04:20:04 -0400

    Added quiz questions and true/false answers for quiz app

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+var questionsArr = [
+      {
+        question: 'JavaScript is super fun to learn? Click OK for True, Cancel for False',
+        answer: true
+      },
+      {
+        question: 'The Earth is flatter than paper? Click OK for True, Cancel for False',
+        answer: false
+      }, 
+      {
+        question: 'Study hard and you will succeed? Click OK for True, Cancel for False',
+        answer: true
+      }, 
+      {
+        question: 'DaVinci believed that simplicity is the ultimate sophistication”? Click OK for True, Cancel for False',
+        answer: true
+      }, 
+      {
+        question: 'E.T. is a friendly alien that wants to phone home? Click OK for True, Cancel for False',
+        answer: true
+      }, 
+      {
+        question: 'HTML stands for HyperText Markup Language? Click OK for True, Cancel for False',
+        answer: true
+      }, 
+      {
+        question: 'Python is a type of snake? (tricky one) Click OK for True, Cancel for False',
+        answer: true
+      }, 
+      {
+        question: 'CSS stands for Cascading Style Sheets? Click OK for True, Cancel for False',
+        answer: true
+      }, 
+];
+
+function runQuiz () {}
